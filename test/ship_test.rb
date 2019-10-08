a300fc8e072fabea0f9695cd308022d4a0495c63
Melissa Robbins <melissamrobbins@gmail.com>
2019-10-08 08:38:48 -0600

    Add tests for hit and sunk?
    Co-Authored-By: Nathan K <nkeller1@users.noreply.github.com>

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -5,8 +5,32 @@ require './lib/ship'
 
 class ShipTest < Minitest::Test
 
+def setup
+  @cruiser = Ship.new("Cruiser", 3)
+end
+
   def test_it_exists
-    cruiser = Ship.new("Cruiser", 3)
-    assert_instance_of Ship, cruiser
+    assert_instance_of Ship, @cruiser
   end
+
+# Do we need this?
+  def test_it_got_hit
+    @cruiser.hit
+    assert_equal 2, @cruiser.health
+  end
+
+# Is this duplicative of above?
+  def test_health_decreases_when_hit
+    @cruiser.hit
+    @cruiser.hit
+    assert_equal 1, @cruiser.health
+  end
+
+  def test_sunk_when_health_eqauls_zero
+    @cruiser.hit
+    @cruiser.hit
+    @cruiser.hit
+    assert_equal true, @cruiser.sunk?
+  end
+
 end
