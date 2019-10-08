5d98fa8e26251dd4ac85ad772abf0fa5508a5733
Nathan Keller <keller.nathan@gmail.com>
2019-10-08 15:16:54 -0600

    added attr_reader and def initialize to cell
    Co-Authored-By: Melissa Robbins <melissamrobbins@gmail.com>

---
+++ lib/cell.rb
@@ -0,0 +1,18 @@
+class Cell
+  attr_reader :coordinate, :ship
+
+  def initialize(coordinate)
+    @coordinate = coordinate
+    @ship = ship
+  end
+
+  def place_ship(ship)
+
+  end
+
+  # def empty?
+  #   if
+  #   end
+  # end
+
+end
