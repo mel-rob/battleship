44efd25c8157ac3a6ee5c5c162539dd7578d5e3c
Nathan Keller <keller.nathan@gmail.com>
2019-10-08 15:53:11 -0600

    added cells to runner file
    Co-Authored-By: Melissa Robbins <melissamrobbins@gmail.com>

--- runner.rb
+++ runner.rb
@@ -1,21 +1,37 @@
 require './lib/ship'
+require './lib/cell'
 
 
 cruiser = Ship.new("Cruiser", 3)
 submarine = Ship.new("Submarine", 2)
+cell_1 = Cell.new("B4")
+cell_2 = Cell.new("C4")
+cell_3 = Cell.new("D4")
 
-p cruiser.name
-p cruiser.length
-p cruiser.health
+
+p cell_1.empty?
 
 
-cruiser.hit
-p cruiser.health
-p cruiser.length
-p cruiser.sunk?
-cruiser.hit
-p cruiser.health
-p cruiser.length
-cruiser.hit
-p cruiser.health
-p cruiser.sunk?
+
+
+
+
+
+
+
+# Testing Ship
+# p cruiser.name
+# p cruiser.length
+# p cruiser.health
+#
+#
+# cruiser.hit
+# p cruiser.health
+# p cruiser.length
+# p cruiser.sunk?
+# cruiser.hit
+# p cruiser.health
+# p cruiser.length
+# cruiser.hit
+# p cruiser.health
+# p cruiser.sunk?
