61e9eee7a6084a430efee34f9fffd9e3efc32493
Melissa Robbins <melissamrobbins@gmail.com>
2019-10-09 08:40:25 -0600

    Add Cell render method

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class Cell
-  attr_reader :coordinate, :ship, :status
+  attr_reader :coordinate, :ship, :fired_upon
 
   def initialize(coordinate)
     @coordinate = coordinate
@@ -22,4 +22,23 @@ class Cell
     @ship = name
   end
 
+  def fire_upon
+    # Do we need health to subtract here?
+    # @health -= 1
+    @fired_upon = true
+  end
+
+  def render(player = true)
+    if player == true && @ship != nil && @fired_upon == false
+      "S"
+    elsif @ship == nil && @fired_upon == true
+      "M"
+    elsif @ship != nil && @fired_upon == true && @ship.health > 0
+      "H"
+    elsif @ship != nil && @fired_upon == true && @ship.health == 0
+      "X"
+    else
+      "."
+    end
+  end
 end
