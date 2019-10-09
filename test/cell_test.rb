8fdb8726696b807191834f6aab5a2be9b6bd2ab2
Melissa Robbins <melissamrobbins@gmail.com>
2019-10-08 18:38:30 -0600

    Add tests to Cell test

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -9,25 +9,33 @@ class CellTest < Minitest::Test
 
   def setup
     @cell = Cell.new("B4")
+    @cruiser = Ship.new("Cruiser", 3)
   end
 
   def test_it_exists
     assert_instance_of Cell, @cell
   end
 
-  def test_coordinate_exists
+  def test_it_has_a_coordinate
     assert_equal "B4", @cell.coordinate
   end
 
-  def test_can_ship_be_in_cell
-    assert_equal
+  def test_ship_starts_as_nil
+    assert_equal nil, @cell.ship
   end
 
-  def test_is_ship_in_cell
+  def test_fired_upon_starts_as_false
+    skip
+    assert_equal false, @cell.fired_upon
   end
 
   def test_cell_empty?
+    skip
     assert_equal true, @cell.empty?
+    cruiser = Ship.new("Cruiser", 3)
+
+    @cell.place_ship(cruiser)
+    assert_equal false, @cell.empty?
   end
 
 
