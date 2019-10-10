4696c573c44b51463f7e0dd54db92c6f2e42da34
Nathan Keller <keller.nathan@gmail.com>
2019-10-10 12:03:07 -0600

    Change @health to attr_accessor
    Co-Authored-By: Melissa Robbins <melissamrobbins@gmail.com>

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,13 +1,13 @@
 class Ship
 
-  attr_reader :name, :length, :health
-  # attr_accessor :health
+  attr_reader :name, :length
+  attr_accessor :health
 
   def initialize(name, length)
     @name = name
     @length = length
     # should @health = length (vs. @length)?
-    @health = @length
+    @health = length
   end
 
   def hit
