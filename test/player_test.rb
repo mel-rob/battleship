8df2c68f17fbc2f59e5f60f36cf6487e8c8ed5a7
Melissa Robbins <melissamrobbins@gmail.com>
2019-10-13 14:03:13 -0600

    Add create_player tests

--- test/player_test.rb
+++ test/player_test.rb
@@ -13,8 +13,25 @@ class PlayerTest < Minitest::Test
     @player = Player.new
   end
 
-
   def test_it_exists
     assert_instance_of Player, @player
   end
+
+  def test_it_has_board
+    @player.create_player
+    assert_instance_of Board, @player.player_board
+  end
+
+  def test_it_has_cruiser
+    @player.create_player
+    assert_instance_of Ship, @player.player_cruiser
+    assert_equal "Cruiser", @player.player_cruiser.name
+  end
+
+  def test_it_has_submarine
+    @player.create_player
+    assert_instance_of Ship, @player.player_submarine
+    assert_equal "Submarine", @player.player_submarine.name
+  end
+
 end
