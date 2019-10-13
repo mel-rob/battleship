5329a930e820e9c11749977dbda3fe3c36e24ace
Melissa Robbins <melissamrobbins@gmail.com>
2019-10-13 14:02:51 -0600

    Add create_player method

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,19 @@
 class Player
 
+  attr_reader :player_cruiser, :player_submarine
+  attr_accessor :player_board
+
+
   def initialize
+    @player_board = player_board
+    @player_cruiser = player_cruiser
+    @player_submarine = player_submarine
+  end
+
+  def create_player
+    @player_board = Board.new
+    @player_cruiser = Ship.new("Cruiser", 3)
+    @player_submarine = Ship.new("Submarine", 2)
   end
 
 end
