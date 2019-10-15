78754020d4496779c978c271a463309592cb0644
Nathan Keller <keller.nathan@gmail.com>
2019-10-15 11:09:21 -0600

    Add generate ship placment test

--- test/computer_player_test.rb
+++ test/computer_player_test.rb
@@ -5,41 +5,41 @@ require './lib/ship'
 require './lib/cell'
 require './lib/board'
 require './lib/computer_player'
-require 'pry'
 
-class ComputerPlayerTest < Minitest::Test
+class ComputerTest < Minitest::Test
 
   def setup
-    @computer_player = ComputerPlayer.new
     @computer_cruiser = Ship.new("Cruiser", 3)
     @computer_submarine = Ship.new("Submarine", 2)
-
+    @computer_board = Board.new
+    @computer = Computer.new(@computer_board)
   end
 
   def test_it_exists
-    assert_instance_of ComputerPlayer, @computer_player
+    assert_instance_of Computer, @computer
   end
 
   def test_it_has_board
-    @computer_player.create_player
-    assert_instance_of Board, @computer_player.computer_board
+    assert_instance_of Board, @computer.computer_board
+  end
+  #
+  # def test_it_has_cruiser
+  #   assert_instance_of Ship, @computer.computer_cruiser
+  #   assert_equal "Cruiser", @computer.computer_cruiser.name
+  # end
+  #
+  # def test_it_has_submarine
+  #   assert_instance_of Ship, @computer.computer_submarine
+  #   assert_equal "Submarine", @computer.computer_submarine.name
+  # end
+
+  def test_generate_ship_placement
+    assert_equal true, @computer_board.valid_placement?(@computer_cruiser, @computer.generate_ship_placement(@computer_cruiser))
   end
 
-  def test_it_has_cruiser
-    @computer_player.create_player
-    assert_instance_of Ship, @computer_player.computer_cruiser
-    assert_equal "Cruiser", @computer_player.computer_cruiser.name
-  end
-
-  def test_it_has_submarine
-    @computer_player.create_player
-    assert_instance_of Ship, @computer_player.computer_submarine
-    assert_equal "Submarine", @computer_player.computer_submarine.name
-  end
-
-  # def test_generate_ship_placement
-  #   @computer_player.generate_ship_placement
+  # def test_it_can_fire_on_cell
   #
   #   assert_equal true,
-  end
+  #test once player_board has been created
+  # end
 end
