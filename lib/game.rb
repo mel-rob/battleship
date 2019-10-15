aac5b3979f18e1a93cf3033fefef8134942c991d
Melissa Robbins <melissamrobbins@gmail.com>
2019-10-15 16:04:20 -0600

    Refactor game to separate methods and runner to loop methods

--- lib/game.rb
+++ lib/game.rb
@@ -17,88 +17,114 @@ class Game
     @computer = Computer.new(@computer_board)
   end
 
-  def start
+  def main_menu
     puts "-" * 50
     puts "Welcome to BATTLESHIP"
     puts "Enter p to play. Enter q to quit."
     puts "-" * 50
     response = gets.chomp[0].upcase
     if response == "P"
-      @computer.place_ship(@computer_cruiser)
-      @computer.place_ship(@computer_submarine)
-      puts "-" * 50
-      puts "I have laid out my ships on the grid."
-      puts "You now need to lay out your two ships."
-      puts "The Cruiser is three units long and the Submarine is two units long."
-      puts "-" * 50
-      puts @player_board.render(true)
-      puts "Enter the squares for the Cruiser (3 spaces):"
-      #have issue with commas between coordinates
-      loop do
-        print "> "
-        #Remove commas?
-        coordinates = gets.chomp.upcase.split(" ")
-        if @player_board.valid_placement?(@player_cruiser, coordinates)
-          @player_board.place(@player_cruiser, coordinates)
-          puts "-" * 50
-          puts @player_board.render(true) + "\n"
-          break
-        else
-          puts "-" * 50
-          puts "Those are invalid coordinates. Please try again:"
-          puts "-" * 50
-        end
-      end
-# Start of Submarine Placment
-      puts "Enter the squares for the Submarine (2 spaces):"
-      loop do
-        print "> "
-        coordinates = gets.chomp.upcase.split(" ")
-        if @player_board.valid_placement?(@player_submarine, coordinates)
-          @player_board.place(@player_submarine, coordinates)
-          puts "-" * 50
-          puts @player_board.render(true) + "\n"
-          puts "************* Ready to play! *************" + "\n"
-          break
-        else
-          puts "-" * 50
-          puts "Those are invalid coordinates. Please try again:"
-          puts "-" * 50
-        end
-      end
-
-        until @player_cruiser.sunk? && @player_submarine.sunk? || @computer_cruiser.sunk? && @computer_submarine.sunk? do
-
-          puts "=============COMPUTER BOARD============="
-          puts @computer_board.render(false)
-          puts "==============PLAYER BOARD=============="
-          puts @player_board.render(true)
-          puts "Enter the coordinate for your shot:"
-          loop do
-            coordinate = gets.chomp.upcase
-            print "> "
-            # binding.pry
-            if @computer_board.valid_coordinate?(coordinate) && @computer_board.cells[coordinate].fired_upon == false
-              @computer_board.cells[coordinate].fire_upon
-              player_shot = coordinate
-              @computer.fire_upon_cell(@player_board)
-              break
-            elsif !@computer_board.valid_coordinate?(coordinate)
-              puts "Please enter a valid coordinate:"
-            else
-              puts "You have already fired on this coordinate. Please choose again:"
-            end
-          end
-        end
-       if @player_cruiser.sunk? && @player_submarine.sunk?
-         puts "I won!"
-         # break
-       elsif @computer_cruiser.sunk? && @computer_submarine.sunk?
-         puts "You won!"
-         # break
-       end
+      board_setup
     elsif response == "Q"
-      puts "You quit! Come back and play later!"
+      puts "You quit! Come back and play again later!"
+    else
+      main_menu
     end
   end
+
+  def board_setup
+    @computer.place_ship(@computer_cruiser)
+    @computer.place_ship(@computer_submarine)
+    puts "-" * 50
+    puts "I have laid out my ships on the grid."
+    puts "You now need to lay out your two ships."
+    puts "The Cruiser is three units long and the Submarine is two units long."
+    puts "-" * 50
+    puts @player_board.render(true)
+    cruiser_setup
+  end
+
+  def cruiser_setup
+    puts "Enter the squares for the Cruiser (3 spaces):" #have issue with commas between coordinates
+    print "> "
+    coordinates = gets.chomp.upcase.split(" ")
+    if @player_board.valid_placement?(@player_cruiser, coordinates)
+      @player_board.place(@player_cruiser, coordinates)
+      puts "-" * 50
+      puts @player_board.render(true) + "\n"
+      submarine_setup
+    else
+      puts "-" * 50
+      puts "Those are invalid coordinates. Please try again:"
+      puts "-" * 50
+      cruiser_setup
+    end
+  end
+
+  def submarine_setup
+    puts "Enter the squares for the Submarine (2 spaces):"
+    print "> "
+    coordinates = gets.chomp.upcase.split(" ")
+    if @player_board.valid_placement?(@player_submarine, coordinates)
+      @player_board.place(@player_submarine, coordinates)
+      puts "-" * 50
+      puts @player_board.render(true) + "\n"
+      puts "************* Ready to play! *************" + "\n"
+      turn
+    else
+      puts "-" * 50
+      puts "Those are invalid coordinates. Please try again:"
+      puts "-" * 50
+      submarine_setup
+    end
+  end
+
+  def turn
+
+
+
+  end
 end
+#
+#         until @player_cruiser.sunk? && @player_submarine.sunk? || @computer_cruiser.sunk? && @computer_submarine.sunk? do
+#
+#           puts "=============COMPUTER BOARD============="
+#           puts @computer_board.render(false)
+#           puts "==============PLAYER BOARD=============="
+#           puts @player_board.render(true)
+#           puts "Enter the coordinate for your shot:"
+#           loop do
+#             coordinate = gets.chomp.upcase
+#             print "> "
+#             if @computer_board.valid_coordinate?(coordinate) && @computer_board.cells[coordinate].fired_upon == false
+#               @computer_board.cells[coordinate].fire_upon
+#               player_shot = coordinate
+#               @computer.fire_upon_cell(@player_board)
+#               break
+#             elsif !@computer_board.valid_coordinate?(coordinate)
+#               puts "Please enter a valid coordinate:"
+#             else
+#               puts "You have already fired on this coordinate. Please choose again:"
+#             end
+#             require "pry"; binding.pry
+#             if player_shot.render == "H"
+#               puts "Your shot on #{coordinate} was a hit!"
+#             # elsif ***.render == "M"
+#             #   puts "Your shot on #{coordinate} was a miss!"
+#             # elsif ***.render == "X"
+#               # puts "Your shot on #{coordinate} sunk their ship!"
+#             end
+#           end
+#         end
+#        if @player_cruiser.sunk? && @player_submarine.sunk?
+#          puts "I won!"
+#          # break
+#        elsif @computer_cruiser.sunk? && @computer_submarine.sunk?
+#          puts "You won!"
+#          # break
+#        end
+#     elsif response == "Q"
+#       puts "You quit! Come back and play later!"
+#     end
+#   end
+# end
--- runner.rb
+++ runner.rb
@@ -1,4 +1,15 @@
 require './lib/game'
 
-game = Game.new
-game.start
+def start
+  loop do
+    game = Game.new
+    game.main_menu
+    game.board_setup
+      until @player_cruiser.sunk? && @player_submarine.sunk? || @computer_cruiser.sunk? && @computer_submarine.sunk?
+        game.turn
+      end
+    game.end
+  end
+end
+
+start
