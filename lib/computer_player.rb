81e9f8ac599f16b9321c31359e18a2fceab801b6
Nathan Keller <keller.nathan@gmail.com>
2019-10-15 11:09:37 -0600

    Add fire_upon_cell method

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -1,31 +1,30 @@
 require 'pry'
 
 
-class ComputerPlayer
+class Computer
 
-  attr_reader :computer_cruiser, :computer_submarine
-  attr_accessor :computer_board
+  attr_reader :computer_board
 
 
-  def initialize
-    @computer_board = Board.new
-    @computer_cruiser = Ship.new("Cruiser", 3)
-    @computer_submarine = Ship.new("Submarine", 2)
+  def initialize(board)
+    @computer_board = board
   end
 
 
-  def generate_ship_placement
+  def generate_ship_placement(ship)
     coordinates = []
-    until @computer_board.valid_placement?(@computer_cruiser, coordinates) == true
-      coordinates = @computer_board.cells.keys.sample(@computer_cruiser.length)
+    until @computer_board.valid_placement?(ship, coordinates) == true
+      coordinates = @computer_board.cells.keys.sample(ship.length)
     end
     coordinates
   end
 
-  def computer_fire_upon
-    #generate a coordinate and fire upon that coordinate.
-    #Needs to look at the player board and make sure it has not taken a shot
-    until @computer_board.valid_placement?(@computer_cruiser, coordinates) == false
-      coordinates = @computer_board.cells.keys.sample(@computer_cruiser.length)
+  def fire_upon_cell
+    coordinate = []
+    until @player_board.cells[coordinate].fired_upon == false
+      coordinate = @player_board.cells.keys.sample
+      @player_board.cells[coordinate].fire_upon
+    end
+    coordinate
   end
 end
