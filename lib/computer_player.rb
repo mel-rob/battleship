57708930c6c12bb3331d1354a45d187e1f4a4d87
Nathan Keller <keller.nathan@gmail.com>
2019-10-14 19:14:49 -0600

    generate ship placement method complete

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -1,3 +1,6 @@
+require 'pry'
+
+
 class ComputerPlayer
 
   attr_reader :computer_cruiser, :computer_submarine
@@ -5,28 +8,24 @@ class ComputerPlayer
 
 
   def initialize
-    @computer_board = computer_board
-    @computer_cruiser = computer_cruiser
-    @computer_submarine = computer_submarine
-  end
-
-  def create_player
     @computer_board = Board.new
     @computer_cruiser = Ship.new("Cruiser", 3)
     @computer_submarine = Ship.new("Submarine", 2)
   end
 
-  def generate_ship_placement(@ship)
-    until @computer_board.valid_placement?(@ship, coordinates) == true
-      coordinates = @computer_board.cells.keys.sample(@ship.length)
+
+  def generate_ship_placement
+    coordinates = []
+    until @computer_board.valid_placement?(@computer_cruiser, coordinates) == true
+      coordinates = @computer_board.cells.keys.sample(@computer_cruiser.length)
     end
     coordinates
-
-    @computer_player = ComputerPlayer.new
-    generate_ship_placement
   end
 
-end
-
-
+  def computer_fire_upon
+    #generate a coordinate and fire upon that coordinate.
+    #Needs to look at the player board and make sure it has not taken a shot
+    until @computer_board.valid_placement?(@computer_cruiser, coordinates) == false
+      coordinates = @computer_board.cells.keys.sample(@computer_cruiser.length)
+  end
 end
