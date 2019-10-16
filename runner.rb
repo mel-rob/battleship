737dd780e929acbd1478623fbb497fc1fb2d460f
Melissa Robbins <melissamrobbins@gmail.com>
2019-10-16 14:17:52 -0600

    Simplify runner to include only game.start
    Co-Authored-By: Nathan K <nkeller1@users.noreply.github.com>

--- runner.rb
+++ runner.rb
@@ -1,13 +1,4 @@
 require './lib/game'
 
-# def start
-#   loop do
-#     game = Game.new
-#     game.main_menu
-#     game.board_setup
-#     game.turn
-#     game.end
-#   end
-# end
 game = Game.new
 game.start
